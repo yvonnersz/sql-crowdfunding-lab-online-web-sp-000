d4265137d7472eceb336cf9c6d6efc2bfbbbd6e7
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-01 06:06:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, projects.funding_goal FROM projects LEFT OUTER JOIN pledges ON projects.id = pledges.project_id WHERE pledges.amount >= projects.funding_goal;"
+  "SELECT projects.title, SUM(pledges.amount)-projects.funding_goal as difference FROM projects LEFT OUTER JOIN pledges ON projects.id = pledges.project_id GROUP BY pledges.project_id HAVING projects.funding_goal <= SUM(pledges.amount);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
